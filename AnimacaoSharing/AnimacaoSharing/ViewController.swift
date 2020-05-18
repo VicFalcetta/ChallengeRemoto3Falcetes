bcfad05dc1fd7e56d4758ab45deb12ce08b23b8e
Victor Falcetta do Nascimento <vic.falcetis@gmail.com>
2020-05-18 15:48:02 -0300

    Commit inicial do projeto

---
+++ AnimacaoSharing/AnimacaoSharing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimacaoSharing
+//
+//  Created by Victor Falcetta do Nascimento on 18/05/20.
+//  Copyright © 2020 Victor Falcetta do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
