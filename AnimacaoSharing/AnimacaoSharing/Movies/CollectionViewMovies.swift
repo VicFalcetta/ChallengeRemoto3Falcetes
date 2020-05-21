816ed2a0f840d7d04330ec0e8fbbcdb6d8a72242
raxanob <rayanexavier161@gmail.com>
2020-05-21 19:10:49 -0300

    Extensions pagina filmes

---
+++ AnimacaoSharing/AnimacaoSharing/Movies/CollectionViewMovies.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewMovies.swift
+//  AnimacaoSharing
+//
+//  Created by Rayane Xavier on 21/05/20.
+//  Copyright © 2020 Victor Falcetta do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+extension MoviesViewController: UICollectionViewDelegate {
+    
+}
+
+extension MoviesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
---
+++ AnimacaoSharing/AnimacaoSharing/Movies/SearchBarMovies.swift
@@ -0,0 +1,17 @@
+//
+//  HelpersMovies.swift
+//  AnimacaoSharing
+//
+//  Created by Rayane Xavier on 21/05/20.
+//  Copyright © 2020 Victor Falcetta do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+extension MoviesViewController: UISearchBarDelegate {
+    
+}
+
+extension MoviesViewController: UISearchControllerDelegate {
+    
+}
