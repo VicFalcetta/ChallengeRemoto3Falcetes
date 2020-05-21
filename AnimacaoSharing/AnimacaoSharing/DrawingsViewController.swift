e12dbf3ac640a160838991673bf9986abf8b7b34
raxanob <rayanexavier161@gmail.com>
2020-05-21 16:19:45 -0300

    Classes

---
+++ AnimacaoSharing/AnimacaoSharing/DrawingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingsViewController.swift
+//  AnimacaoSharing
+//
+//  Created by Rayane Xavier on 21/05/20.
+//  Copyright © 2020 Victor Falcetta do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class DrawingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ AnimacaoSharing/AnimacaoSharing/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  AnimacaoSharing
+//
+//  Created by Rayane Xavier on 21/05/20.
+//  Copyright © 2020 Victor Falcetta do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
