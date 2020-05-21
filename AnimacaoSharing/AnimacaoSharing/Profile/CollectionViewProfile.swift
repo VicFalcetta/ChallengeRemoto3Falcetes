dc8ecdef164986eeec160619a1be4a9329f952f0
raxanob <rayanexavier161@gmail.com>
2020-05-21 19:32:53 -0300

    Extension pagina Profile

---
+++ AnimacaoSharing/AnimacaoSharing/Profile/CollectionViewProfile.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewProfile.swift
+//  AnimacaoSharing
+//
+//  Created by Rayane Xavier on 21/05/20.
+//  Copyright © 2020 Victor Falcetta do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+extension ProfileViewController: UICollectionViewDelegate {
+    
+}
+
+extension ProfileViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //
+    }
+}
