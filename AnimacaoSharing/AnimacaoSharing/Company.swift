3a27ece9d8aa29d1ae5bf8e37e57695325de6135
raxanob <rayanexavier161@gmail.com>
2020-05-21 23:11:47 -0300

    Criação do struct Company

---
+++ AnimacaoSharing/AnimacaoSharing/Company.swift
@@ -0,0 +1,18 @@
+//
+//  Company.swift
+//  AnimacaoSharing
+//
+//  Created by Rayane Xavier on 21/05/20.
+//  Copyright © 2020 Victor Falcetta do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+struct Company {
+    var logo: UIImage
+    var name: String
+    var country: String
+    var foundation: String
+    var awards: [String]?
+    var description: String
+}
