82803d5524deb04111a26788c444af515d3c106e
raxanob <rayanexavier161@gmail.com>
2020-05-21 23:06:26 -0300

    Criação do struct animation

---
+++ AnimacaoSharing/AnimacaoSharing/Animation.swift
@@ -0,0 +1,20 @@
+//
+//  Animation.swift
+//  AnimacaoSharing
+//
+//  Created by Rayane Xavier on 21/05/20.
+//  Copyright © 2020 Victor Falcetta do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+struct Animation {
+    var poster: UIImage
+    var title: String
+    var subtitle: String?
+    var grade: String
+    var description: String
+    var genre: [String]
+    var producer: String
+    var release: String
+}
