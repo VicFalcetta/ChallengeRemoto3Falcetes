a3039a9a80665d517e328500edabe174693cf8e1
raxanob <rayanexavier161@gmail.com>
2020-05-21 23:18:27 -0300

    criação do struct Person

---
+++ AnimacaoSharing/AnimacaoSharing/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  AnimacaoSharing
+//
+//  Created by Rayane Xavier on 21/05/20.
+//  Copyright © 2020 Victor Falcetta do Nascimento. All rights reserved.
+//
+
+import UIKit
+
+struct Person {
+    var photo: UIImage
+    var name: String
+    var favoriteCategoriesImage: [UIImage]
+    var favoriteCategories: [String]?
+}
