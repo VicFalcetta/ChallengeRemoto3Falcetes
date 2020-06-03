a80d0451e430020ddbba11e24967cebc0c83b28d
raxanob <rayanexavier161@gmail.com>
2020-06-03 14:59:09 -0300

    retirando alguns cast

--- AnimacaoSharing/AnimacaoSharing/Controller/OnboardingViewController.swift
+++ AnimacaoSharing/AnimacaoSharing/Controller/OnboardingViewController.swift
@@ -17,8 +17,12 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     var scrollHeight: CGFloat = 0.0
     //Dados que serão apresentados no Onboarding de tutorial
     let titles: [String] = ["Welcome to AnimU", "Add animations to your list!", "See their plot and info!"]
-    let descriptions: [String] = ["AnimU is an app that helps you keep track of which animated movies or series that you have watched.","With AnimU you can register an animation to a list of animated movies or animated TV series. Just touch on the '+' icon!", "To see more info about the animation that you added touch on the cell in the table!"]
+    let descriptions: [String] = ["AnimU is an app that helps you keep track of which animated movies or series that you have watched.",
+                                  "With AnimU you can register an animation to a list of movies or TV series. Just touch on the '+' icon!",
+                                  "To see more info about the animation that you added touch on the cell in the table!"]
     let images: [String] = ["onboarding_1", "onboarding_2", "onboarding_3"]
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+
     //Pegando de forma dinâmica a largura e altura da scroll
     override func viewDidLayoutSubviews() {
         scrollWidth = scrollOnboarding.frame.size.width
@@ -28,20 +32,40 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .dark
         self.view.layoutIfNeeded()
-        self.scrollOnboarding.delegate = self
+        scrollOnboarding.delegate = self
         scrollOnboarding.isPagingEnabled = true
         scrollOnboarding.showsVerticalScrollIndicator = false
         scrollOnboarding.showsHorizontalScrollIndicator = false
-        buttonOnboarding.setTitleColor(#colorLiteral(red: 0.07843137255, green: 1, blue: 0.9254901961, alpha: 1), for: .normal)
-        buttonOnboarding.backgroundColor = .clear
-        buttonOnboarding.setTitle("Get Started", for: .normal)
-        buttonOnboarding.isHidden = true
-        buttonOnboarding.layer.cornerRadius = 5
-        buttonOnboarding.layer.borderWidth = 3
-        buttonOnboarding.layer.borderColor = #colorLiteral(red: 0.07843137255, green: 1, blue: 0.9254901961, alpha: 1)
-        buttonOnboarding.layer.masksToBounds = true
+        displayedView()
+        //Mudando o tamanho para caber as views criadas dentro da scroll
+        scrollOnboarding.contentSize = CGSize(width: scrollWidth * CGFloat(titles.count), height: scrollHeight)
+        //Disabilitando o scroll vertical
+        self.scrollOnboarding.contentSize.height = 1.0
+        pageControlOnboarding.numberOfPages = titles.count
+        pageControlOnboarding.currentPage = 0
+    }
+    @IBAction func pageChanged(_ sender: Any) {
+        scrollOnboarding!.scrollRectToVisible(CGRect(x: scrollWidth * CGFloat((pageControlOnboarding?.currentPage)!),
+                                                     y: 0, width: scrollWidth, height: scrollHeight), animated: true)
+    }
+    @IBAction func buttonGetStarted(_ sender: UIButton) {
+        Onboarding.shared.setIsNotNewUser()
+        dismiss(animated: true, completion: nil)
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        setIndicatorForCurrentPage()
+        if pageControlOnboarding.currentPage == 2 {
+            buttonOnboarding.isHidden = false
+        } else {
+            buttonOnboarding.isHidden = true
+        }
+    }
+    func setIndicatorForCurrentPage() {
+        let page = (scrollOnboarding?.contentOffset.x)! / scrollWidth
+        pageControlOnboarding?.currentPage = Int(page)
+    }
+    func displayedView() {
         //Criar a view que será apresentada
-        var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
         for index in 0..<titles.count {
             frame.origin.x = scrollWidth * CGFloat(index)
             frame.size = CGSize(width: scrollWidth, height: scrollHeight)
@@ -69,30 +93,17 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
             slide.addSubview(txtDescription)
             scrollOnboarding.addSubview(slide)
         }
-        //Mudando o tamanho para caber as views criadas dentro da scroll
-        scrollOnboarding.contentSize = CGSize(width: scrollWidth * CGFloat(titles.count), height: scrollHeight)
-        //Disabilitando o scroll vertical
-        self.scrollOnboarding.contentSize.height = 1.0
-        pageControlOnboarding.numberOfPages = titles.count
-        pageControlOnboarding.currentPage = 0
+        setButtonOnboarding()
     }
-    @IBAction func pageChanged(_ sender: Any) {
-        scrollOnboarding!.scrollRectToVisible(CGRect(x: scrollWidth * CGFloat((pageControlOnboarding?.currentPage)!), y: 0, width: scrollWidth, height: scrollHeight), animated: true)
+    func setButtonOnboarding() {
+        buttonOnboarding.setTitleColor(#colorLiteral(red: 0.07843137255, green: 1, blue: 0.9254901961, alpha: 1), for: .normal)
+        buttonOnboarding.backgroundColor = .clear
+        buttonOnboarding.setTitle("Get Started", for: .normal)
+        buttonOnboarding.isHidden = true
+        buttonOnboarding.layer.cornerRadius = 5
+        buttonOnboarding.layer.borderWidth = 3
+        buttonOnboarding.layer.borderColor = #colorLiteral(red: 0.07843137255, green: 1, blue: 0.9254901961, alpha: 1)
+        buttonOnboarding.layer.masksToBounds = true
     }
-    @IBAction func buttonGetStarted(_ sender: UIButton) {
-        Onboarding.shared.setIsNotNewUser()
-        dismiss(animated: true, completion: nil)
-    }
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        setIndicatorForCurrentPage()
-        if pageControlOnboarding.currentPage == 2 {
-            buttonOnboarding.isHidden = false
-        } else {
-            buttonOnboarding.isHidden = true
-        }
-    }
-    func setIndicatorForCurrentPage() {
-        let page = (scrollOnboarding?.contentOffset.x)! / scrollWidth
-        pageControlOnboarding?.currentPage = Int(page)
-    }
+
 }
